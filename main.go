e8132c36c1b2eeda61d7f36c3d94ab8aaa9a9e27
Akshay Khanna <akshaykhanna@Akshays-MacBook-Pro.local>
2021-02-22 14:56:16 +0530

    Constantly check health of websites with delay of 5 seconds b/w each iteration of check by creating seprate go-routines of each link check & using channel to communicate b/w parent & child routines

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,16 +14,25 @@ func main() {
 		"https://twitter.com",
 		"https://www.beartobull.in",
 	}
+	channel := make(chan string)
 	for _, link := range webSites {
-		checkLink(link)
+		go checkLink(link, channel)
+	}
+	for channelOutput := range channel {
+		go func(link string) {
+			time.Sleep(time.Second * 5)
+			checkLink(link, channel)
+		}(channelOutput)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Printf("I think %s is down :/ Error: %d \n", link, err)
+		c <- link
 		return
 	}
 	fmt.Printf("%s is up & running fine :)\n", link)
+	c <- link
 }
